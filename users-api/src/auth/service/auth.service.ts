7d5c14721f0d2760e5fbe813ea9882716dff60e2
Kristian Hadzhikolev <balccbf@gmail.com>
2022-10-10 17:33:57 +0200

    auth service with hash and salt

---
+++ users-api/src/auth/service/auth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const bcrypt = require('bcryptjs');
+
+@Injectable()
+export class AuthService {
+    constructor(private readonly jwtService: JwtService) {}
+
+    generateJWT(user): Promise<string> {
+        return this.jwtService.signAsync(
+            { user },
+            { secret: process.env.JWT_SECRET, expiresIn: '1d' },
+        );
+    }
+
+    async hashSaltPassword(password: string): Promise<string> {
+        const saltRounds = 10;
+        return bcrypt.hash(password, saltRounds);
+    }
+
+    comparePasswords(password: string, passwordHash: string): Promise<boolean> {
+        return bcrypt.compare(password, passwordHash);
+    }
+}
