45a811b80edf8eee53b359a8de8cddcd5ad132c5
Kristian Hadzhikolev <balccbf@gmail.com>
2022-12-20 00:06:36 +0100

    rating-api added missing deps, fixed .env loading, moved back to http microservice

--- rating-api/src/app.module.ts
+++ rating-api/src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RatingModule } from './rating/rating.module';
 import {AuthModule} from "./auth/auth.module";
+import {configService} from "./config.service";
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [RatingModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+    RatingModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- rating-api/src/rating/controller/rating.controller.ts
+++ rating-api/src/rating/controller/rating.controller.ts
@@ -6,22 +6,18 @@ import {
   Get,
   Inject,
   InternalServerErrorException,
-  Post,
-  UseFilters,
+  Post, Put,
+  UseFilters, UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
-import {
-  Ctx,
-  EventPattern,
-  MessagePattern,
-  Payload,
-  RmqContext,
-  // @ts-ignore
-} from '@nestjs/microservices';
 import { RatingExceptionInterceptor } from '../../interceptors/rating-exception.interceptor';
 import { Rating } from '../../database/entities/rating.entity';
 import { RatingService } from '../service/rating.service';
 import { RatingValue } from '../models/ratingValue.dto';
+import {Role} from "../../auth/enumerator/roles.enum";
+import {JwtAuthGuard} from "../../auth/guards/jwt.guard";
+import {hasRole} from "../../auth/decorators/role.decorator";
+import {RolesGuard} from "../../auth/guards/roles.guard";
 
 @Controller('rating')
 @UseFilters(new HttpExceptionFilter())
@@ -30,16 +26,17 @@ import { RatingValue } from '../models/ratingValue.dto';
 export class RatingController {
   constructor(private ratingService: RatingService) {}
 
-  // @hasRole(Role.User)
-  // @UseGuards(JwtAuthGuard, RolesGuard)
-  // @Get()
-  @EventPattern('createRating')
-  async createRating(data) {
+  @hasRole(Role.User)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Post('')
+  async createRating(@Body() data) {
     return await this.ratingService.create(data.id, data.userId, data.rating);
   }
 
-  @EventPattern('changeRating')
-  async changeRating(data) {
+  @hasRole(Role.User)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Put('')
+  async changeRating(@Body() data) {
     console.log('data', data);
     return await this.ratingService.changeRating(
       data.id,
@@ -48,26 +45,17 @@ export class RatingController {
     );
   }
 
-  @EventPattern('getUserRating')
-  async getUserRating(data) {
+  @hasRole(Role.User)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Get('userRating')
+  async getUserRating(@Body() data) {
     return await this.ratingService.getUserRating(data.id, data.userId);
   }
 
-  @MessagePattern('ratingValue')
-  async getRatingCount(@Payload() data): Promise<RatingValue> {
+  @hasRole(Role.User)
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Get('ratingCount')
+  async getRatingCount(@Body() data): Promise<RatingValue> {
     return await this.ratingService.getRatingValue(data.id);
   }
-
-  // @MessagePattern('ratingCount')
-  // async getRatingValue(
-  //   @Payload() data: string,
-  // ): Promise<number> {
-  //   // console.log(this.ratingService.getRatingCount(data));
-  //   return this.ratingService.getRatingCount(data);
-  // }
-
-  // @EventPattern('ratingAverage')
-  // async getRatingAverage(data: string) {
-  //   return await this.ratingService.getRatingOverall(data);
-  // }
 }
