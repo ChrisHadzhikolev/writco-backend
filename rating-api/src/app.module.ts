8502a789f0276fed2e2cce9065156d353f9bc2bc
Kristian Hadzhikolev <balccbf@gmail.com>
2022-12-19 22:36:26 +0100

    rating-api small change

--- rating-api/src/app.module.ts
+++ rating-api/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RatingModule } from './rating/rating.module';
+import {AuthModule} from "./auth/auth.module";
 
 @Module({
-  imports: [],
+  imports: [RatingModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
